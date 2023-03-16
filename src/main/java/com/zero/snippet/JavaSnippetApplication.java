9141572295e1d3e4e48e899f5a56a23b57707db0
lishaofei <zerolee1993@163.com>
2023-03-16 15:48:18 +0800

    init

---
+++ src/main/java/com/zero/snippet/JavaSnippetApplication.java
@@ -0,0 +1,13 @@
+package com.zero.snippet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSnippetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSnippetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zero/snippet/JavaSnippetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.snippet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSnippetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
